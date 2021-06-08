2f840bc94f733a9151e39314e5825a19e31489f5
adrewn <huuhung1999@gmail.com>
2021-06-07 23:23:08 -0400

    Finished responsive, sticky nav bar and srolling view

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -41,3 +41,41 @@ const slider = function () {
 };
 
 slider();
+
+/*Sticky bar navigation*/
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+/*Page navigation*/
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+document.querySelector(".btn-show-me").addEventListener("click", function (e) {
+  e.preventDefault();
+  document
+    .querySelector(".section-services")
+    .scrollIntoView({ behavior: "smooth" });
+});
